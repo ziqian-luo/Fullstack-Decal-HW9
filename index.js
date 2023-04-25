a7f15b6676feafed2712bf13b2da7c5f53ab06f4
Ziqian Luo <ziqian.luo@berkeley.edu>
2023-04-24 23:33:26 -0700

    fds

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const user = require("./routes/user");
+const InitiateMongoServer = require("./config/db");
+
+// Initiate Mongo Server
+InitiateMongoServer();
+
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
+});
+
+// /**
+//  * Router Middleware
+//  * Router - /user/*
+//  * Method - *
+//  */
+//  app.use("/user", user);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+// export model User with UserSchema
+module.exports = mongoose.model("User", UserSchema);
